ae4e948e9c809ef69271bed5cc798a88e61bc4c6
tedd02 <dtted02@gmail.com>
2025-12-01 17:20:21 +0100

    project setup ( Router & Controller )

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 //importation du modèle Thing
 const Thing = require('./models/thing');
 
+//importation des routes
+const stuffRoutes = require('./routes/stuff');
+
 const app = express();
 
 mongoose
@@ -21,38 +24,41 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+//using routes for products
+app.use('/api/stuff', stuffRoutes);
 
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+// app.post('/api/stuff', (req, res, next) => {
+//   delete req.body._id;
+//   const thing = new Thing({
+//     ...req.body
+//   });
+//   thing.save()
+//     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+//     .catch(error => res.status(400).json({ error }));
+// });
 
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
+// app.get('/api/stuff', (req, res, next) => {
+//   Thing.find()
+//     .then(things => res.status(200).json(things))
+//     .catch(error => res.status(400).json({ error }));
+// });
 
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+// app.get('/api/stuff/:id', (req, res, next) => {
+//   Thing.findOne({ _id: req.params.id })
+//     .then(thing => res.status(200).json(thing))
+//     .catch(error => res.status(404).json({ error }));
+// });
 
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+// app.put('/api/stuff/:id', (req, res, next) => {
+//   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+//     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+//     .catch(error => res.status(400).json({ error }));
+// });
+
+// app.delete('/api/stuff/:id', (req, res, next) => {
+//   Thing.deleteOne({ _id: req.params.id })
+//     .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+//     .catch(error => res.status(400).json({ error }));
+// });
 
 module.exports = app;
