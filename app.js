c41a2cf4ed5711937bc11e54954c379134ede68c
tedd02 <dtted02@gmail.com>
2025-12-02 01:12:58 +0100

    Auth update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Thing = require('./models/thing');
 
 //importation des routes
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -22,10 +23,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-});
+}); 
 
 //using routes for products
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 // app.post('/api/stuff', (req, res, next) => {
 //   delete req.body._id;
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // importing models
-const Thing = require('../models/thing');
+const auth = require('../middleware/auth');
 
 //importing controller
 const stuffCtrl = require('../controllers/stuff');
 
 
-router.get('/', stuffCtrl.getAllStuff);
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 // router.post('/', (req, res, next) => {
 //   const thing = new Thing({
